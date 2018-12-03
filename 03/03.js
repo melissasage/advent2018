510bade4eb029d9b4e1906a90052166e6be86e3e
Melissa Sage Dumont <melissadumont91@gmail.com>
2018-12-03 10:02:58 -0500

    First pass at solution to 03-01

--- 03/03.js
+++ 03/03.js
@@ -4,4 +4,36 @@ const puzzleInput = fs
   .readFileSync("03/input.txt", "utf-8", error => console.error(error))
   .split(/\n/);
 
-const parseData = datum => {};
+const parseData = datum => {
+  /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/.test(datum);
+  return {
+    id: +RegExp.$1,
+    fromLeft: +RegExp.$2,
+    fromTop: +RegExp.$3,
+    width: +RegExp.$4,
+    height: +RegExp.$5
+  };
+};
+
+let puzzleFabric = new Array(1000).fill(new Array(1000).fill(0));
+
+const claimFabric = data => fabric => {
+  for (let x = 0; x < data.length; x++) {
+    const datum = parseData(data[x]);
+    console.log(datum);
+    for (let row = datum.fromLeft; row < datum.fromLeft + datum.width; row++) {
+      for (let col = datum.fromTop; col < datum.fromTop + datum.height; col++) {
+        console.log(`square before change: ${fabric[row][col]}`);
+        fabric[col][row] = 1;
+        console.log(
+          `this ran: row=${row} and col=${col} and fabric=${fabric[row][col]}`
+        );
+        console.log(fabric);
+      }
+    }
+  }
+};
+
+const countOverlapping = () => {};
+
+module.exports = { parseData, claimFabric, countOverlapping };
--- 03/03.spec.js
+++ 03/03.spec.js
@@ -1,14 +1,33 @@
 const { expect } = require("chai");
-const { parseData } = require("./03.js");
+const { parseData, claimFabric, countOverlapping } = require("./03.js");
 
 describe("Day 03: No Matter How You Slice It", () => {
-  xit(`parseData() turns a string into a JavaScript object`, () => {
+  let fabric;
+  beforeEach(() => {
+    fabric = new Array(10).fill(new Array(10).fill(0));
+  });
+
+  it(`parseData() turns a string into a correctly formatted JavaScript object`, () => {
     const data = parseData(`#123 @ 3,2: 5x4`);
     expect(typeof data).to.equal(`object`);
     expect(data.id).to.equal(123);
     expect(data.fromLeft).to.equal(3);
-    expect(data.fromRight).to.equal(2);
+    expect(data.fromTop).to.equal(2);
     expect(data.width).to.equal(5);
     expect(data.height).to.equal(4);
   });
+  it(`claimFabric() increments indices representing claimed squares`, () => {
+    claimFabric([`#1 @ 1,3: 4x4`])(fabric);
+    expect(fabric[1][3]).to.equal(1);
+    expect(fabric[3][4]).to.equal(1);
+    expect(fabric[1][0]).to.equal(0);
+
+    claimFabric([`#2 @ 3,1: 4x4`, `#3 @ 5,5: 2x2`])(fabric);
+
+    expect(fabric[3][4]).to.equal(2);
+    expect(fabric[1][4]).to.equal(1);
+  });
+  xit(
+    `countOverlapping() returns the number of square inches of fabric with multiple claims`
+  );
 });
